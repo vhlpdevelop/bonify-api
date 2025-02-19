1c95848e9c1365d59e8402bce27f3efdc6b4fc9e
Victor Hugo Lopes Poli <vhlp.develop@gmail.com>
2025-02-18 21:37:01 -0400

    v0.1.3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
   apps: [
     {
       name: `HotSpot Service`,
-      script: 'build/src/hotspot_service/app.js',
-      instances: "max",
+      script: './src/hotspot_service/app.js',
+     
       exec_mode: "cluster",
     },
     {
       name: `API GATEWAY`,
-      script: 'build/src/app.js',
-      instances: "max",
+      script: './src/app.js',
+ 
       exec_mode: "cluster",
     },
   ]
